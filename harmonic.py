d0bae8b361b781b6505fa3c2857def7df4f92a0b
davestroud <david@greymatter-analytics.com>
2018-03-16 22:30:21 -0500

    Commit

--- harmonic.py
+++ harmonic.py
@@ -1,11 +1,13 @@
 import stdio
 import sys
 
+
 def harmonic(n):
     total = 0.0
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         total += 1.0 / float(i)
     return total
+
 
 for j in range(1, len(sys.argv)):
     arg = int(sys.argv[j])
