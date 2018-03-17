91be40acc711dbfb000a50171f0f4d639c3052ce
davestroud <david@greymatter-analytics.com>
2018-03-17 11:42:46 -0500

    Just testing github

---
+++ playground.py
@@ -0,0 +1,17 @@
+# Import the numpy package as np
+import numpy as np
+
+# Create list baseball
+baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+
+# Create a numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out type of np_baseball
+print(type(np_baseball))
+
+print(baseball)
+
+out = np.array([0, 0])
+np.sum([[1, 2], [3, 4]], 0, out=out)
+print(out)
